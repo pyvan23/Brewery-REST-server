30920252ee2674b7b6d94f1e1c5587d51ceb73fe
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-14 21:07:43 -0300

    validate jwt

--- models/user.js
+++ models/user.js
@@ -34,7 +34,8 @@ const UserSchema = Schema({
 
 
 UserSchema.methods.toJSON = function () {
-    const { __v, password, ...user } = this.toObject()
-    return user;
+    const { __v, password, _id, ...user } = this.toObject()
+    user.uid = _id
+    return { user };
 }
 export default model('User', UserSchema) 
