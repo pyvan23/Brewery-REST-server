dcea4244c3ef309606f7c7a8dcd7769c75202d82
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-16 20:16:02 -0300

    validate user state is in false

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -9,19 +9,24 @@ export const validateJwt = async (req, res, next) => {
     const token = req.header('x-token');
     console.log(token);
     if (!token) {
-        console.log(token);
+        
         return res.status(401).json({ msg: 'request have not token' })
     }
 
     try {
 
         const { uid } = jwt.verify(token, process.env.SECRET_KEY);
+        //read user uid to know
         const userAuthenticated = await User.findById(uid);
+        //verify if user state is in true or false
+        if (!userAuthenticated.state) {
+            return res.status(401).json({msg:'Token invalid - user state false '});
+        }
 
         req.user = userAuthenticated
 
         next()
-        
+
     } catch (error) {
 
         res.status(401).json({ msg: 'token invalid' })
