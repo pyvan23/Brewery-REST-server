df054899e5efc119d526f91da94681f86fc5636f
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-17 22:39:20 -0300

    test 1

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -7,10 +7,10 @@ import User from "../models/user.js";
 export const validateJwt = async (req, res, next) => {
 
     const token = req.header('x-token');
-    
+
     if (!token) {
 
-        return res.status(401).json({ msg: 'request have not token' })
+        return res.status(401).json({ msg: 'request have not token,unauthorized error' })
     }
 
     try {
@@ -18,18 +18,17 @@ export const validateJwt = async (req, res, next) => {
         const { uid } = jwt.verify(token, process.env.SECRET_KEY);
 
         //read user uid to know if is register in DB
-        const user  = await User.findById(uid);
+        const user = await User.findById(uid);
         //verify if user state is in true or false in DB
-
-        console.log( user );
+        
         if (!user) {
-            return res.status(401).json({ msg: 'user was deleted ' });
+            return res.status(401).json({ msg: 'user was deleted, ' });
         }
         if (!user.state) {
             return res.status(401).json({ msg: 'Token invalid - user state false ' });
         }
         //create a property in the request,so i can acces to the request body with user info
-        req.user = user; 
+        req.user = user;
 
         next()
 
--- routes/users.js
+++ routes/users.js
@@ -46,12 +46,17 @@ userRouter.post(
 );
 
 userRouter.delete("/:id",
-    [   validateJwt,
+    [
+
+        validateJwt,
         isAdminRole,
         check("id", "this id is not valid").isMongoId(),
         check("id").custom(isUserExist),
         validateFields
-    ],  deleteUsers);
+
+    ],
+
+    deleteUsers);
 
 userRouter.patch("/", patchUsers);
 
