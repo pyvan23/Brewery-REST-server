3af235622e0502f13ed246203c150092323aac55
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-19 09:25:53 -0300

    another middleware to roles

--- middlewares/validate-roles.js
+++ middlewares/validate-roles.js
@@ -20,8 +20,24 @@ export const isAdminRole = (req, res, next) => {
 
 }
 //...rest when its use in the arguments if not are spread operator
-export const haveRol = (...rest) => {
+//...rest will be an array
+export const haveRol = (...rols) => {
 
+    return (req, res, next) => {
+
+        if (!req.user) {
+            return res.status(500).json({ msg: 'someone wants to verify without validate token first' });
+        }
+
+        if (!rols.includes(req.user.rol)) {
+
+            return res.status(401).json({
+                msg: `this service needs this roles ${rols}`
+            })
+        }
+
+        next()
+    }
 
 
 }
