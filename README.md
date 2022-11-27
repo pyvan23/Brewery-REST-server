54381b46958a4ea3898eb4b4136dd8fb2d9e4f6b
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-27 20:09:05 -0300

    README searching endpoints

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 npm install
 ```
 
-# Documentation
+# Docs
 
+Users - Categories - Products - endpoints
 ```
 https://documenter.getpostman.com/view/19000107/2s8YmUKKG7
 ```
+
 
 
--- controllers/searching.js
+++ controllers/searching.js
@@ -1,10 +1,95 @@
+import { isValidObjectId } from "mongoose";
+import Category from "../models/category.js";
+import Product from "../models/product.js";
+import User from "../models/user.js";
 
+
+const validCollections = ['products', 'categories', 'users']
+
+const lookingForUser = async (term = '', res) => {
+    const isMongoId = isValidObjectId(term)
+
+    if (isMongoId) {
+        const user = await User.findById(term);
+        return res.json({ results: (user) ? [user] : [] })
+    }
+    const regex = new RegExp(term, 'i')
+
+    const users = await User.find(
+        {
+            $or:
+                [{ name: regex, state: true },
+                {
+                    email: regex, state: true
+                }]
+        }
+    )
+    res.json({ results: users })
+
+}
+const lookingForCategories = async (term = '', res) => {
+    const isMongoId = isValidObjectId(term)
+
+    if (isMongoId) {
+        const category = await Category.findById(term);
+        return res.json({ results: (category) ? [category] : [] })
+    }
+    const regex = new RegExp(term, 'i')
+
+    const categories = await Category.find(
+        {
+            name: regex,
+        }
+    )
+    res.json({ results: categories })
+
+}
+const lookingForProducts = async (term = '', res) => {
+    const isMongoId = isValidObjectId(term)
+
+    if (isMongoId) {
+        const product = await Product.findById(term)
+            .populate('category', 'name')
+        return res.json({ results: (product) ? [product] : [] })
+    }
+    const regex = new RegExp(term, 'i')
+
+    const products = await Product.find(
+        {
+            name: regex,
+        }
+    ).populate('category', 'name')
+    res.json({ results: products })
+
+}
 
 export const searching = (req, res) => {
 
     const { collection, term } = req.params;
 
-    res.status(200).json(`${collection} ${term}` );
+    if (!validCollections.includes(collection)) {
+
+        return res.status(400).json({ msg: `The valid collection are ${validCollections}` })
+    }
+
+    switch (collection) {
+        case 'products':
+            lookingForProducts(term, res);
+            break;
+        case 'categories':
+            lookingForCategories(term, res);
+            break;
+        case 'users':
+            lookingForUser(term, res);
+            break;
+
+        default:
+            res.status(500).json({ msg: 'internal server error' })
+
+    }
+
+
+
 
 
 }
